d4444f449755b98cb85ebe61681094024477db81
stickies-v <stickies-v@protonmail.com>
2023-11-08 20:45:23 +0000

    reduce nesting
    if allowed_cmd_opts is a valid iterator, by definition there have to be
    OptionsCategory::COMMAND_OPTIONS arguments, so we can simplify that logic

--- src/common/args.cpp
+++ src/common/args.cpp
@@ -682,14 +682,14 @@ std::string ArgsManager::GetHelpMessage() const
 
                 if (category == OptionsCategory::COMMANDS) {
                     const auto allowed_cmd_opts{m_command_args.find(arg_name)};
-                    const auto all_cmd_opts{m_available_args.find(OptionsCategory::COMMAND_OPTIONS)};
-                    if (all_cmd_opts != m_available_args.end() && allowed_cmd_opts != m_command_args.end()) {
-                        for (const auto& cmd_opt_name : allowed_cmd_opts->second) {
-                            const auto& cmd_opt{all_cmd_opts->second.at(cmd_opt_name)};
-                            if ((cmd_opt.m_flags & ArgsManager::DEBUG_ONLY) && !show_debug) continue;
-                            usage += HelpMessageOpt(cmd_opt_name, cmd_opt.m_help_param, cmd_opt.m_help_text, true);
-                         }
-                    }
+                    if (allowed_cmd_opts == m_command_args.end()) continue;
+
+                    const auto& all_cmd_opts{m_available_args.at(OptionsCategory::COMMAND_OPTIONS)};
+                    for (const auto& cmd_opt_name : allowed_cmd_opts->second) {
+                        const auto& cmd_opt{all_cmd_opts.at(cmd_opt_name)};
+                        if ((cmd_opt.m_flags & ArgsManager::DEBUG_ONLY) && !show_debug) continue;
+                        usage += HelpMessageOpt(cmd_opt_name, cmd_opt.m_help_param, cmd_opt.m_help_text, true);
+                        }
                 }
             }
         }
