563b9da94984c222ec79e26cea0eb1f655ca23c4
stickies-v <stickies-v@protonmail.com>
2025-05-21 12:52:10 +0100

    kernel: correctly define BITCOINKERNEL_BUILD

--- src/kernel/bitcoinkernel_c.cpp
+++ src/kernel/bitcoinkernel_c.cpp
@@ -2,6 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#define BITCOINKERNEL_BUILD
+
 #include <kernel/bitcoinkernel.h>
 
 #include <kernel/bitcoinkernel.hpp>
